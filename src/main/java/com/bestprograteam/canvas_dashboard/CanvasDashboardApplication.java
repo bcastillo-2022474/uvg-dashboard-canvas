62defeb7e0b609e3c93ed43027d48808e654d4d3
Joao Castillo <bcastillo-2022474@kinal.edu.gt>
2025-09-11 23:13:01 -0600

    config: sobre esta roca edificare mi iglesia (Mateo 16:18)

---
+++ src/main/java/com/bestprograteam/canvas_dashboard/CanvasDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.bestprograteam.canvas_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanvasDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanvasDashboardApplication.class, args);
+	}
+
+}
