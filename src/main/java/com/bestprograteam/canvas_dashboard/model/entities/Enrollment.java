3b9c7e173ef3bbc36e27ad7d2961906be7816805
Sergio <edu.vela2005@gmail.com>
2025-09-12 23:57:45 -0600

    feat: create Enrollment entity with basic fields

---
+++ src/main/java/com/bestprograteam/canvas_dashboard/model/entities/Enrollment.java
@@ -0,0 +1,6 @@
+package com.bestprograteam.canvas_dashboard.model.entities;
+
+public class Enrollment {
+    private Integer courseId;              // Reference to Course
+    private String enrollmentState;
+}
