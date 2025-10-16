11c048f1d295f645b6742fc9a96434a40fd58e25
luciajuracanmoran <luciajuracanmoran556@gmail.com>
2025-10-16 10:09:26 -0600

    feat(service): Implementar estructura inicial de PredictionService

---
+++ src/main/java/com/bestprograteam/canvas_dashboard/model/entities/services/PredictionService.java
@@ -0,0 +1,10 @@
+package com.bestprograteam.canvas_dashboard.model.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PredictionService {
+    
+    // En esta primera versión solo definimos la estructura básica del servicio.
+    
+}
